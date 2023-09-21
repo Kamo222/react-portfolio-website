d950a912d2b56a1ee1de57d8e943a28a150a10ef
kamo222 <kamohelo.mamaregane1@gmail.com>
2023-09-21 02:54:22 +0200

    Initial Commit

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,17 @@
+import LogoTitle from '../../assets/images/logo-s.png'
+
+const Home = () => {(
+    <div className="container home-page">
+        <div className="text-zone">
+            <h1>Hi, <br /> I'm
+            <img src={LogoTitle} alt='developer' />
+            kamohelo
+            <br />
+            web developer
+            </h1>
+            <h2>Full Stack Developer</h2>
+        </div>
+    </div>
+)}
+
+export default Home
